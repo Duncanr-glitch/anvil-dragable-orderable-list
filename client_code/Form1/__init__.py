1e74a7514f1ee9587a633557deb92b37c43a2e81
duncan_richards12@yahoo.com <duncan_richards12@yahoo.com>
2024-02-18 14:32:16 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -40,6 +40,7 @@ class Form1(Form1Template):
   def form_show(self, **event_args):
     """This method is called when the form is shown on the page"""
     self.orderable_list_1.components = [ListItem(item_text="Hello Init", index=0)]
+    print(self.orderable_list_1.remove_button_properties)
     self.orderable_list_1.remove_button_properties = {
       "background": "red",
       "foreground": "yellow"
